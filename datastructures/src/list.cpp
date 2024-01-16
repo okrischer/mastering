714b851d7a88da43df43dfd4c7fef033d89689c5
Oliver Krischer <oliver.krischer@gmail.com>
2024-01-16 17:06:26 +0100

    added simple tests for datastructures/list

--- datastructures/src/list.cpp
+++ datastructures/src/list.cpp
@@ -1,188 +1,44 @@
+#include "list.hpp"
 #include <iostream>
-#include <stdexcept>
 
-// A generic container for storing one element of the given type
-// and a pointer to the next element.
-template<typename T>
-class Node {
-public:
-  T data;
-  Node* next;
-
-  Node(int d, Node* n) : data{d}, next{n} {}
-  ~Node() {}
-
-  void print() {
-    if (!next) std::cout << data << "\n";
-    else {
-      std::cout << data << " -> ";
-      next->print();
-    }
+void test_stack() {
+  std::cout << "Testing the stack interface...\n";
+  auto stack = Stack<int>();
+  stack.push(3);
+  stack.push(2);
+  stack.push(1);
+  stack.print();
+  std::cout << "top element: " << stack.peek() << "\n";
+  std::cout << "stack size: " << stack.size() << "\n";
+  std::cout << "popped " << stack.pop() << "\n";
+  std::cout << "popped " << stack.pop() << "\n";
+  std::cout << "popped " << stack.pop() << "\n";
+  stack.print();
+  std::cout << "stack size: " << stack.size() << "\n";
+  try {
+    auto top = stack.peek();
+  } catch (std::out_of_range& err) {
+    std::cerr << err.what() << "\n";
   }
-};
+}
 
-// The puplic List interface, implemented by all lists.
-template<typename T>
-class List {
-public:
-  virtual ~List() {}
-  virtual int size() const = 0;
-  virtual void print() const = 0;
-  virtual T peek() const = 0;
-};
-
-// A singly linked list with references to the head and the tail
-// of the list.
-template<typename T>
-class LinkedList : public List<T> {
-public:
-  LinkedList() : head{nullptr}, tail{nullptr}, sz{0} {}
-  ~LinkedList() {
-    Node<T>* node = head;
-    while(node) {
-      Node<T>* curr = node;
-      node = node->next;
-      delete curr;
-    }
+void test_queue() {
+  std::cout << "Testing the queue interface...\n";
+  auto queue = Queue<int>();
+  queue.enqueue(1);
+  queue.enqueue(2);
+  queue.enqueue(3);
+  queue.print();
+  std::cout << "next element: " << queue.peek() << "\n";
+  std::cout << "queue size: " << queue.size() << "\n";
+  std::cout << "dequeued " << queue.dequeue() << "\n";
+  std::cout << "dequeued " << queue.dequeue() << "\n";
+  std::cout << "dequeued " << queue.dequeue() << "\n";
+  queue.print();
+  std::cout << "queue size: " << queue.size() << "\n";
+  try {
+    auto next = queue.peek();
+  } catch (std::out_of_range& err) {
+    std::cerr << err.what() << "\n";
   }
-
-  // Returns the current size of the list.
-  int size() const override {return sz;}
-
-  // Prints the current list.
-  void print() const override {
-    if (!head) std::cout << "empty\n";
-    else head->print();
-  }
-
-  // Returns the head of the list.
-  // If the list is empty, an out_of_range exception is thrown.
-  T peek() const override {
-    if (head) return head->data;
-    else throw std::out_of_range{"connot peek from empty list"};
-  }
-
-  // Inserts a new element at the head of the list.
-  void insertHead(T elem) {
-    head = new Node<T>(elem, head);
-    if (!tail) tail = head;
-    sz++;
-  }
-
-  // Inserts a new element at the tail of the list.
-  void insertTail(T elem) {
-    if (!tail) insertHead(elem);
-    else {
-      tail->next = new Node<T>(elem, nullptr);
-      tail = tail->next;
-      sz++;
-    }
-  }
-
-  // Removes and returns the head of the list.
-  // If the list is empty, an out_of_range exception is thrown.
-  T removeHead() {
-    if (head) {
-      Node<T>* first = head;
-      T elem = head->data;
-      head = head->next;
-      delete first;
-      sz--;
-      return elem;
-    } else throw std::out_of_range{"connot remove from empty list"};
-  }
-
-private:
-  Node<T>* head;
-  Node<T>* tail;
-  int sz;
-};
-
-// Stack implements a LIFO data structure based on linked lists.
-// LIFO means `Last In - First Out`, i.e. the last added element
-// is the first to be retrieved.
-template<typename T>
-class Stack : public List<T> {
-public:
-  Stack() {
-    stack = new LinkedList<T>;
-  }
-  ~Stack() {
-    delete stack;
-  }
-
-  // Returns the current size of the stack.
-  int size() const override {
-    return stack->size();
-  }
-
-  // Prints the current stack.
-  void print() const override {
-    stack->print();
-  }
-
-  // Returns the top element of the stack.
-  // If the stack is empty, an out_of_range exception is thrown.
-  T peek() const override {
-    return stack->peek();
-  }
-
-  // Pushes a new element on top of the stack.
-  void push(T elem) {
-    stack->insertHead(elem);
-  }
-
-  // Removes and returns the top element of the stack.
-  // If the stack is empty, an out_of_range exception is thrown.
-  T pop() {
-    return stack->removeHead();
-  }
-
-private:
-  LinkedList<T>* stack;
-};
-
-// Queue implements a FIFO data structure based on linked lists.
-// FIFO means `First In - First Out`, i.e. the first added element
-// is the first to be retrieved.
-template<typename T>
-class Queue : public List<T> {
-public:
-  Queue() {
-    queue = new LinkedList<T>;
-  }
-  ~Queue() {
-    delete queue;
-  }
-
-  // Returns the current size of the queue.
-  int size() const override {
-    return queue->size();
-  }
-
-  // Prints the current queue.
-  void print() const override {
-    queue->print();
-  }
-
-  // Returns the head of the queue.
-  // If the queue is empty, an out_of_range exception is thrown.
-  T peek() const override {
-    return queue->peek();
-  }
-
-  // Enqueues a new element at the tail of the queue.
-  void enqueue(T elem) {
-    queue->insertTail(elem);
-  }
-
-  // Removes and returns the head of the queue.
-  // If the queue is empty, an out_of_range exception is thrown.
-  T dequeue() {
-    return queue->removeHead();
-  }
-
-private:
-  LinkedList<T>* queue;
-};
-
+}
---
+++ tests/list_test.cpp
@@ -0,0 +1,8 @@
+#include "list.hpp"
+#include <iostream>
+
+int main() {
+  test_stack();
+  std::cout << "\n";
+  test_queue();
+}
